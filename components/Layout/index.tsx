7a374ecab88685fcbc70bc83a69cc515d8bfddd5
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-03-24 00:05:03 +0800

    fix:

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,24 +1,12 @@
 import Navbar from "../NavBar";
 import Footer from "../Footer";
-import { useEffect, useState } from "react";
-import { getLayoutDom } from "../../utils/elementUtils";
 
 export default function Layout({ children }) {
-  const [loading, setLoading] = useState<boolean>(true);
-  // 绑定全局获取元素事件
-  useEffect(() => {
-    getLayoutDom();
-    setLoading(false);
-  }, []);
   return (
     <>
-      {!loading && (
-        <>
-          <Navbar />
-          {children}
-          <Footer />
-        </>
-      )}
+      <Navbar />
+      {children}
+      <Footer />
     </>
   );
 }
--- utils/elementUtils.ts
+++ utils/elementUtils.ts
@@ -1,75 +1,72 @@
 // 全局滚动的盒子
-export let layoutContent: any = null;
+export const layoutContent: any = () => {
+    return document.getElementById('__next');
+};
 // 全局导航盒子
-export let layoutNav: any = null;
+export const layoutNav: any = () => {
+    return document.getElementById('layout_nav');
+};
 // 当前的主题
 export let theme: number = 1;
 // 记录上一次滚动的位置
 let lastScrollPos = 0;
-export let time;
-
-// 初始化获取全局元素
-export const getLayoutDom = () => {
-    layoutContent = document.getElementById('__next');
-    layoutNav = document.getElementById('layout_nav');
-};
 
 // 取消全局导航的样式
 export const removeLayoutNavStyle = () => {
-    if (!layoutNav) return;
-    layoutNav?.classList.remove('nav_active');
+    if (!layoutNav()) return;
+    layoutNav()?.classList.remove('nav_active');
 };
 
 // 设置全局导航样式
 export const addLayoutNavStyle = () => {
-    if (!layoutNav) return;
-    layoutNav?.classList.add('nav_active');
+    if (!layoutNav()) return;
+    layoutNav()?.classList.add('nav_active');
 };
 
 // 设置全局导航样式
 export const addNavItemStyle = () => {
     const domList = Array.from(document.querySelectorAll('.nav_item_text'));
     domList.forEach((v: any) => v?.classList.add('nav_item_block'));
-    if (!layoutNav) return;
-    layoutNav?.classList.add('nav_active_border');
+    if (!layoutNav()) return;
+    layoutNav()?.classList.add('nav_active_border');
 };
 
 // 取消全局导航样式
 export const removeNavItemStyle = () => {
     const domList = Array.from(document.querySelectorAll('.nav_item_text'));
     domList.forEach((v: any) => v?.classList.remove('nav_item_block'));
-    if (!layoutNav) return;
-    layoutNav?.classList.remove('nav_active_border');
+    if (!layoutNav()) return;
+    layoutNav()?.classList.remove('nav_active_border');
 };
 
 // 页面滚动事件
 export const pageScroll = () => {
-    if (!layoutNav) return;
-    if (layoutContent?.scrollTop && layoutContent?.scrollTop > 50) {
-        layoutNav?.classList.add('nav_active');
-        if (lastScrollPos - layoutContent?.scrollTop > 30) {
-            lastScrollPos = layoutContent?.scrollTop
-            layoutNav?.classList.remove('nav_none');
+    if (!layoutNav() || !layoutContent()) return;
+    if (layoutContent()?.scrollTop && layoutContent().scrollTop > 50) {
+        layoutNav()?.classList.add('nav_active');
+        if (lastScrollPos - layoutContent()?.scrollTop > 30) {
+            lastScrollPos = layoutContent()?.scrollTop
+            layoutNav()?.classList.remove('nav_none');
         }
-        if (layoutContent?.scrollTop > 30 && layoutContent?.scrollTop - lastScrollPos > 30) {
-            lastScrollPos = layoutContent?.scrollTop
-            layoutNav?.classList.add('nav_none');
+        if (layoutContent()?.scrollTop > 30 && layoutContent()?.scrollTop - lastScrollPos > 30) {
+            lastScrollPos = layoutContent()?.scrollTop
+            layoutNav()?.classList.add('nav_none');
         }
     } else {
-        layoutNav?.classList.remove('nav_active');
+        layoutNav()?.classList.remove('nav_active');
     }
 };
 
 // 设置页面滚动事件
 export const bindHandleScroll = () => {
-    if (!layoutContent) return;
-    layoutContent.addEventListener('scroll', pageScroll, false);
+    if (!layoutContent()) return;
+    layoutContent().addEventListener('scroll', pageScroll, false);
 };
 
 // 卸载页面滚动事件
 export const removeScroll = () => {
-    if (!layoutContent) return;
-    layoutContent.removeEventListener('scroll', pageScroll, false);
+    if (!layoutContent()) return;
+    layoutContent().removeEventListener('scroll', pageScroll, false);
     lastScrollPos = 0;
 };
 
