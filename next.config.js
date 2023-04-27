dc09e55117281e0a4ba2021b1e64cb9d46c527b2
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-04-27 15:39:24 +0800

    fix: 修改next配置文件

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const getRobotsPath = () => path.join(__dirname, "public", "robots.txt");
 const getSitemapPath = () => path.join(__dirname, "public", "sitemap.xml");
+const getDeadChainPath = () => path.join(__dirname, "public", "deadChain.xml");
 
 const nextConfig = {
   reactStrictMode: true,
@@ -16,20 +17,25 @@ const nextConfig = {
       },
     ],
   },
-  serverless: {
-    pages: {
-      "/api/*": {
-        endpoint: "pages/api/*",
+  async rewrites() {
+    return [
+      {
+        source: "/api",
+        destination: "/",
       },
-    },
-  },
-  withNextEnv: {
-    getRobotsTxtOptions: () => ({
-      filePath: getRobotsPath(),
-    }),
-    getServerRuntimeConfig: () => ({
-      sitemapPath: getSitemapPath(),
-    }),
+      {
+        source: "/robots.txt",
+        destination: getRobotsPath(),
+      },
+      {
+        source: "/sitemap.xml",
+        destination: getSitemapPath(),
+      },
+      {
+        source: "/deadChain.xml",
+        destination: getDeadChainPath(),
+      },
+    ];
   },
 };
 
