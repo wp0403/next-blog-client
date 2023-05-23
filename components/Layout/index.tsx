43c88b766d4ede9a1ef52ed6fa52e53de1158eba
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-05-23 14:34:47 +0800

    Update index.tsx

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,8 +1,15 @@
 import Navbar from "../NavBar";
 import Footer from "../Footer";
 import { LayoutContextProvider } from "@store/layoutStore";
+import { useEffect } from "react";
 
 export default function Layout({ children }) {
+  const getIp = async () => {
+    const res = await fetch(`https://wp-boke.work/api/getIp`);
+  };
+  useEffect(() => {
+    getIp();
+  }, []);
   return (
     <>
       <LayoutContextProvider>
