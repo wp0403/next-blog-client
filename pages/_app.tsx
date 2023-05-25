c41ca887efe8c7990bc7570d70f37f02e3d6e11e
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-05-25 23:54:11 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,6 +35,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="baidu-site-verification" content="codeva-ZUhqyitPRt" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
       <Layout>
         {loading ? <PageLoading /> : <Component {...pageProps} />}
