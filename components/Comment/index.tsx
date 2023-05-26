7d936b5175184c1274e8b344aa972448e57a4423
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-05-26 11:41:43 +0800

    Update index.tsx

--- components/Comment/index.tsx
+++ components/Comment/index.tsx
@@ -4,7 +4,7 @@
  * @Author: WangPeng
  * @Date: 2023-05-25 18:37:40
  * @LastEditors: WangPeng
- * @LastEditTime: 2023-05-26 10:36:08
+ * @LastEditTime: 2023-05-26 11:29:19
  */
 import React, { useEffect } from "react";
 import { isBrowser } from "@/utils/elementUtils";
@@ -22,7 +22,7 @@ const Comment = (props: Props) => {
         const _window: any = window;
         _window.twikoo = twikoo;
         _window.twikoo.init({
-          envId: "http://wp-boke.work:8080", // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+          envId: "https://comment.wp-boke.work", // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
           el: "#tcomment", // 容器元素
           // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
           // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
