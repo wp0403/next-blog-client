cb775437978a81f19053dab1799664c1355307d2
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-08-24 15:34:22 +0800

    Update index.tsx

--- components/RanderMarkdownIt/index.tsx
+++ components/RanderMarkdownIt/index.tsx
@@ -4,5 +4,12 @@
  * @Author: WangPeng
  * @Date: 2023-08-24 10:49:42
  * @LastEditors: WangPeng
- * @LastEditTime: 2023-08-24 10:49:43
+ * @LastEditTime: 2023-08-24 15:34:09
  */
+import React from "react";
+
+const RanderMarkdownIt = () => {
+  return <div></div>;
+};
+
+export default RanderMarkdownIt;
