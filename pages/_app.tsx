559a6e3d762086a9ac088a795fb4d10ffae830ac
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-09-01 15:51:17 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,13 +38,28 @@ export default function App({ Component, pageProps }) {
     };
   }, []);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // 在客户端环境中执行延迟加载的 JavaScript 代码
+      (window as any).LA.init({
+        id: "3FmNQHwzFUzdFjX3",
+        ck: "3FmNQHwzFUzdFjX3",
+        autoTrack: true,
+        hashMode: true,
+      });
+    }
+  }, []);
+
   return (
     <>
-      <Script id="LA5">
-        {`
-          !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3FmNQHwzFUzdFjX3",ck:"3FmNQHwzFUzdFjX3",autoTrack:true,hashMode:true});
-        `}
-      </Script>
+      <Script
+        id="LA_COLLECT"
+        src="https://wp-1302605407.cos.ap-beijing.myqcloud.com/js/js-sdk-pro.min.js"
+      ></Script>
+      <Script
+        id="LA_EVENT"
+        src="https://wp-1302605407.cos.ap-beijing.myqcloud.com/js/js-sdk-event.min.js"
+      ></Script>
       <Head>
         <meta name="baidu-site-verification" content="codeva-ZUhqyitPRt" />
         <meta
